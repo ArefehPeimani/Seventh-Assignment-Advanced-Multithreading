b776c74bfe71a530a5922552396d279087401fc7
ArefehPeimani <arefehpeimani@gmail.com>
2023-05-13 04:39:11 +0330

    Completed PrioritySimulator and Semaphore

--- src/main/java/sbu/cs/Semaphore/Operator.java
+++ src/main/java/sbu/cs/Semaphore/Operator.java
@@ -1,20 +1,26 @@
 package sbu.cs.Semaphore;
 
-public class Operator extends Thread {
+import java.util.concurrent.Semaphore;
 
-    public Operator(String name) {
+public class Operator extends Thread {
+    Semaphore semaphore;
+    public Operator(String name, Semaphore semaphore) {
         super(name);
+        this.semaphore = semaphore;
     }
 
     @Override
     public void run() {
         for (int i = 0; i < 10; i++)
         {
+            semaphore.acquireUninterruptibly();
             Resource.accessResource();         // critical section - a Maximum of 2 operators can access the resource concurrently
             try {
                 sleep(500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
+            } finally {
+                semaphore.release();
             }
         }
     }
--- src/main/java/sbu/cs/Semaphore/Resource.java
+++ src/main/java/sbu/cs/Semaphore/Resource.java
@@ -1,9 +1,12 @@
 package sbu.cs.Semaphore;
 
+import static java.lang.Thread.currentThread;
+
 public class Resource {
 
     public static void accessResource() {
         try {
+            System.out.println(currentThread().getName() + " : " + System.currentTimeMillis());
             Thread.sleep(100);
         } catch (InterruptedException e) {
             e.printStackTrace();
